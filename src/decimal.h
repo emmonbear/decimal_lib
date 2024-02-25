ad82da4b636ee6c9c1567acc6381ffa98f2d6401
emmonbear <moskaleviluak@icloud.com>
2024-02-25 22:29:35 +0500

    feat: add main header

---
+++ src/decimal.h
@@ -0,0 +1,19 @@
+/**
+ * @file decimal.h
+ * @author emmonbea (https://github.com/emmonbear)
+ * @brief Main header
+ * @version 0.1
+ * @date 2024-02-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef DECIMAL_H
+#define DECIMAL_H
+
+#include "./modules/debug/debug.h"
+#include "./modules/include/bit_lib.h"
+#include "./modules/include/common.h"
+#include "./modules/include/macros.h"
+
+#endif  // DECIMAL_H
