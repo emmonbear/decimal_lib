a7c04ceafcf44ace6201550e58cce3c090a0efbb
emmonbear <moskaleviluak@icloud.com>
2024-02-25 22:25:53 +0500

    fear: add debug header

---
+++ src/modules/debug/debug.h
@@ -0,0 +1,14 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stddef.h>
+
+#include "../include/common.h"
+
+void s21_print_binary_decimal(decimal_t value);
+void s21_print_bit(int number, int color);
+void s21_print_bits(size_t const size, void const *const ptr, int color);
+void s21_print_int128_for_calc(decimal_t value);
+void s21_print_binary_uint192(uint192_t value);
+
+#endif  // DEBUG_H
