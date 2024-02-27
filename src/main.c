7b8a3cfc2ad035930bc804fb95b28d0023a90ec3
kossadda <gabilov1997@gmail.com>
2024-02-27 17:40:45 +0500

    feat: add printd function for printing decimal values

--- src/main.c
+++ src/main.c
@@ -3,14 +3,15 @@
 #include "./decimal.h"
 #include "stdbool.h"
 
+#define MINUS 0x80000000
+#define E 0x10000
 
 int main()
 {
-  uint192_t value_1 = SET_ZERO;
+  decimal_t value_1 = {{0x1, 0x0, 0x0, 0x0, 0x0, 0x0, (E * 60) | MINUS}};
 
   // uint192_t value_2= {{2321}};
-
-  s21_print_binary_uint192(value_1);
+  printd(value_1, "\n");
   // s21_print_binary_uint192(value_2);
 
   // uint192_t res = binary_mul(value_1, value_2);
--- src/modules/debug/debug.h
+++ src/modules/debug/debug.h
@@ -10,5 +10,6 @@ void s21_print_bit(int number, int color);
 void s21_print_bits(size_t const size, void const *const ptr, int color);
 void s21_print_int128_for_calc(decimal_t value);
 void s21_print_binary_uint192(uint192_t value);
+void printd(decimal_t value, char *new_line);
 
 #endif  // DEBUG_H
