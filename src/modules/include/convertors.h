6accf24b0c77368b67875abdae278be0cb5830b4
emmonbear <moskaleviluak@icloud.com>
2024-02-28 20:37:22 +0500

    feat: convertors module

--- src/modules/include/convertors.h
+++ src/modules/include/convertors.h
@@ -1,18 +1,17 @@
 /**
  * @file convertors.h
  * @author kossadda (https://github.com/kossadda)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-02-27
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef CONVERTERS_H_
 #define CONVERTERS_H_
 
-#include "./bit_lib.h"
 #include "./common.h"
 
 typedef enum {
@@ -22,4 +21,4 @@ typedef enum {
 
 // int s21_from_float_to_decimal(float src, decimal_t *dst);
 
-#endif // CONVERTERS_H_
+#endif  // CONVERTERS_H_
