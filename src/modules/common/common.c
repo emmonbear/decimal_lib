c81edd05c1b3b4d0d7ffc56c54cc61800c70e043
emmonbear <moskaleviluak@icloud.com>
2024-03-01 23:40:38 +0500

    feat: add is_correct function

--- src/modules/common/common.c
+++ src/modules/common/common.c
@@ -11,10 +11,12 @@
 
 #include "../include/common.h"
 
+#include "../include/comparison.h"
+
 uint192_t bank_rouding(uint192_t integer, uint192_t fractional) {
   uint192_t res = LDCML_ZERO;
 
-  if (binary_compare(fractional, POINT_FIVE) == EQUAL) {
+  if (s21_is_equal(uint192_to_decimal(fractional), POINT_FIVE)) {
     if (IS_EVEN(integer)) {
       for (uint8_t i = 0; i < (LDEC_SIZE - 1); i++) {
         res.Lbits[i] = integer.Lbits[i];
@@ -22,7 +24,7 @@ uint192_t bank_rouding(uint192_t integer, uint192_t fractional) {
     } else {
       res = binary_add(integer, ONE);
     }
-  } else if (binary_compare(fractional, POINT_FIVE) == GREATER) {
+  } else if (s21_is_greater(uint192_to_decimal(fractional), POINT_FIVE)) {
     res = binary_add(integer, ONE);
   } else {
     res = integer;
@@ -34,4 +36,18 @@ uint192_t bank_rouding(uint192_t integer, uint192_t fractional) {
 s21_decimal dabs(s21_decimal value) {
   value.bits[DEC_SIZE - 1] = (value.bits[DEC_SIZE - 1] << 1) >> 1;
   return value;
+}
+
+/**
+ * @brief
+ *
+ * @param value
+ * @retval 0 - incorrect decimal
+ * @retval 1 - correct decimal
+ */
+int is_correct(s21_decimal value) {
+  unsigned int empty = value.bits[DEC_SIZE - 1] & 0x7F00FFFF;
+  uint8_t power = GET_POWER(value.bits[DEC_SIZE - 1]);
+
+  return ((empty) || (power > 28)) ? 0 : 1;
 }
