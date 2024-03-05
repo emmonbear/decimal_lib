9d7048b29fba747a7d06cb0f553c7dc524358300
kossadda <gabilov1997@gmail.com>
2024-03-05 21:35:50 +0500

    feat: add tests for is_not_equal

--- src/tests/main_test.h
+++ src/tests/main_test.h
@@ -16,8 +16,8 @@
 // #define TEST_SUB
 // #define TEST_MUL
 // #define TEST_DIV
-#define TEST_IS_EQUAL
-// #define TEST_NOT_EQUAL
+// #define TEST_IS_EQUAL
+#define TEST_NOT_EQUAL
 // #define TEST_IS_LESS
 // #define TEST_LESS_OR_EQUAL
 // #define TEST_IS_GREATER
