597ca316f75c9c95a5a2cac860043ff1e4797fc6
kossadda <gabilov1997@gmail.com>
2024-03-05 22:28:18 +0500

    feat: add tests for is_less_or_equal

--- src/tests/main_test.h
+++ src/tests/main_test.h
@@ -18,8 +18,8 @@
 // #define TEST_DIV
 // #define TEST_IS_EQUAL
 // #define TEST_NOT_EQUAL
-#define TEST_IS_LESS
-// #define TEST_LESS_OR_EQUAL
+// #define TEST_IS_LESS
+#define TEST_LESS_OR_EQUAL
 // #define TEST_IS_GREATER
 // #define TEST_GREATER_OR_EQUAL
 // #define TEST_INT_TO_DECIMAL
